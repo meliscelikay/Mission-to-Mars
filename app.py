307f6a0f089d9b31e42196bae593c20b499b8af1
mlsclky <102884313+mlsclky@users.noreply.github.com>
2022-06-19 00:45:13 -0400

    upload Mission_to_Mars

--- app.py
+++ app.py
@@ -1,116 +1,24 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "720e6615",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "[WDM] - ====== WebDriver manager ======\n",
-      "[WDM] - Current google-chrome version is 102.0.5005\n",
-      "[WDM] - Get LATEST chromedriver version for 102.0.5005 google-chrome\n",
-      "[WDM] - Driver [/Users/meliscelikay/.wdm/drivers/chromedriver/mac64/102.0.5005.61/chromedriver] found in cache\n"
-     ]
-    }
-   ],
-   "source": [
-    "from flask import Flask, render_template, redirect, url_for\n",
-    "from flask_pymongo import PyMongo\n",
-    "import scraping"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "6b507256",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "app = Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "7e18bb8b",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Use flask_pymongo to set up mongo connection\n",
-    "app.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/mars_app\"\n",
-    "mongo = PyMongo(app)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "id": "f463b60f",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route(\"/\")\n",
-    "def index():\n",
-    "   mars = mongo.db.mars.find_one()\n",
-    "   return render_template(\"index.html\", mars=mars)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "id": "48467ea3",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route(\"/scrape\")\n",
-    "def scrape():\n",
-    "   mars = mongo.db.mars\n",
-    "   mars_data = scraping.scrape_all()\n",
-    "   mars.update({},mars_data, upsert=True)\n",
-    "   return \"Scraping Successful!\""
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "58616db3",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "if __name__ == \"__main__\":\n",
-    "    app.run(debug=True)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "PythonData",
-   "language": "python",
-   "name": "pythondata"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__) 
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({},mars_data, upsert=True)
+   return "Scraping Successful!"
+
+if __name__ == "__main__":
+   app.run(debug=True)    
