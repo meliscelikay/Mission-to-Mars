d4178662850286390426d962612176fcb166c129
mlsclky <102884313+mlsclky@users.noreply.github.com>
2022-06-19 21:13:16 -0400

    final upload Mission_to_Mars_Challenge

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping
-
-app = Flask(__name__) 
-
+app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
-
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
@@ -17,8 +14,8 @@ def index():
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update({},mars_data, upsert=True)
-   return "Scraping Successful!"
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run(debug=True)    
+   app.run()
--- scraping.py
+++ scraping.py
@@ -98,7 +98,23 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-if __name__ == "__main__":
+def hemispheres(browser):
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
 
-    # If running as script, print scraped data
+    hemisphere_image_urls = []
+
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.find_link_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
+
+if __name__ == "__main__":
     print(scrape_all())
